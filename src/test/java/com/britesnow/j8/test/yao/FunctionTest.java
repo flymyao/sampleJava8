f4dfb255e13bb0254e1b499b701f623cf5542498
flymyao <flymyao@gmail.com>
2014-08-13 17:06:09 +0800

    .add Consumer and predicate test

--- src/test/java/com/britesnow/j8/test/yao/FunctionTest.java
+++ src/test/java/com/britesnow/j8/test/yao/FunctionTest.java
@@ -1,5 +1,8 @@
 package com.britesnow.j8.test.yao;
 
+import java.util.function.Consumer;
+import java.util.function.Predicate;
+
 import org.junit.Test;
 
 public class FunctionTest {
@@ -16,4 +19,26 @@ public class FunctionTest {
 		System.out.println("TestMyFunction (adds,minus) => (3 - 1) + 3 = " + minus.andThen(adds,3).apply(3,1));
 	}
 
+	@Test
+    public void testConsumer(){
+	    Consumer<Integer> consumer = System.out::println;
+	    consumer.accept(1);
+	    System.out.println("==========");
+	    Consumer<Integer> consumer1 = t -> {
+	        System.out.println("Do something!");
+	    };
+	    consumer.andThen(consumer1).accept(2);
+    }
+	
+	@Test
+	public void testPredicate(){
+	    Predicate<Integer> predicate = t -> { return t >= 0 ? true : false;};
+	    System.out.println("judge if t>=0: "+predicate.test(0));
+	    Predicate<Integer> predicate1 = t -> { return t % 2 == 0 ? true : false;};
+	    System.out.println("return the negate result:: "+predicate.negate().test(3));
+	    System.out.println("judge if predicate && predicate1 is true: "+predicate.and(predicate1).test(3));
+	    System.out.println("judge if predicate || predicate1 is true: "+predicate.or(predicate1).test(1));
+	    System.out.println("judge if t>=0 and 2.equal(2): "+Predicate.isEqual(2).test(2));
+	    System.out.println("judge if t>=0 and 2.equal(1): "+Predicate.isEqual(2).test(1));
+	}
 }
